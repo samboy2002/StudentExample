605f7b848a79280e9d7962989c4431c21950603e
samboy2002 <samboy@osjava.net>
2023-03-07 09:28:13 +1300

    Week 2 homework: Student class and linked list

---
+++ IntegerLinkedList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentExample
+{
+    internal class IntegerLinkedList
+    {
+        private IntegerNode? _head;
+        private IntegerNode? _tail;
+
+        private int _length = 0;
+        private int _min = int.MaxValue;
+        private int _max = int.MinValue;
+        public int Length { get { return _length; } }
+        public int Min { get { return _min; } }
+        public int Max { get { return _max; } }
+
+        public void Append(int value)
+        {
+            if (_head == null)
+            {
+                _head = new(value);
+            }
+            else
+            {
+                IntegerNode newNode = new(value);
+
+                _tail ??= _head;
+
+                _tail.Next = newNode;
+                _tail = newNode;
+            }
+
+            _length++;
+            if (value > _max) _max = value;
+            if (value < _min) _min = value;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new();
+            builder.Append("IntegerLinkedList: [");
+
+            for (IntegerNode? node= _head; node!=null; node = node.Next)
+            {
+                builder.Append(node.Value);
+                if (node.Next != null) builder.Append(", ");
+            }
+
+            builder.Append(']');
+
+            return builder.ToString();
+        }
+    }
+
+    internal class IntegerNode
+    {
+        private int _value;
+        private IntegerNode? _next;
+
+        public IntegerNode(int value)
+        {
+            _value = value;
+        }
+
+        public int Value { get { return _value; } set {  _value = value; } }
+        public IntegerNode? Next { get { return _next; } set { _next = value; } }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@ using StudentExample;
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("=== Demonstrate Class Definition ===");
-Student ThisStudent = new Student();
+Student ThisStudent = new();
 ThisStudent.FirstName = "Dapeng";
 ThisStudent.LastName = "LI";
 
-Student AnotherStudent = new Student("1234");
+Student AnotherStudent = new("1234");
 AnotherStudent.FirstName = "Din";
 AnotherStudent.LastName = "Diesel";
 
@@ -16,5 +16,16 @@ ThisStudent.RegisterPaper("ISCG6426");
 AnotherStudent.RegisterPaper("6426");
 bool AttendStatus = ThisStudent.AttendClass();
 
-Console.WriteLine();
+Console.WriteLine('\n');
 Console.WriteLine("=== Demonstrate Linked List ===");
+
+IntegerLinkedList list = new();
+list.Append(1);
+list.Append(2);
+list.Append(3);
+list.Append(4);
+list.Append(5);
+
+Console.WriteLine(list.ToString());
+Console.WriteLine($"The length of the list is {list.Length}");
+Console.WriteLine($"The MaxValue={list.Max}, the MinValue={list.Min}");
