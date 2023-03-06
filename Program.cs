6f7ec054887a46f5cdfbe5862107707462dbeb36
samboy <samboy@SAMBOY7C7F>
2023-03-07 08:43:24 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StudentExample;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("=== Demonstrate Class Definition ===");
+Student ThisStudent = new Student();
+ThisStudent.FirstName = "Dapeng";
+ThisStudent.LastName = "LI";
+
+Student AnotherStudent = new Student("1234");
+AnotherStudent.FirstName = "Din";
+AnotherStudent.LastName = "Diesel";
+
+ThisStudent.RegisterPaper("ISCG6426");
+AnotherStudent.RegisterPaper("6426");
+bool AttendStatus = ThisStudent.AttendClass();
+
+Console.WriteLine();
+Console.WriteLine("=== Demonstrate Linked List ===");
---
+++ Student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentExample
+{
+    internal class Student
+    {
+        private string _address;
+        private string _dob;
+        private string _firstName;
+        private char _gender;
+        private string _lastName;
+        private string _id;
+
+        public Student()
+        {
+            Console.WriteLine("Create a new instance of Student class without student id");
+        }
+
+        public Student(string studentId)
+        {
+            _id = studentId;
+            Console.WriteLine($"Create a new instance of Student class with student id {_id}");
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public string Dob
+        {
+            get { return _dob; }
+            set { _dob = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public char Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public void RegisterPaper(string paperId)
+        {
+            Console.WriteLine($"The student {FirstName} {LastName} register the papaer {paperId}");
+        }
+
+        public void RegisterPaper(int paperCode)
+        {
+            Console.WriteLine($"The student {FirstName} {LastName} register the papaer {paperCode}");
+        }
+
+        public bool AttendClass()
+        {
+            Console.Write($"The student {FirstName} {LastName} attends a class");
+            return true;
+        }
+    }
+}
